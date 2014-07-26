f93a0c7416b505386a4683ced58447df06909165
Zen4All <info@zen4all.nl>
2014-07-26 23:54:27 +0200

    Rewriting to follow Zen Cart Standards
    -moving missing_images.php to admin folder
    -removeing (IMHO) the overcomplicated database connection, ZC already
    facilitates in this.
    -Moving defines to language file
    -moving css to seperate file
    -added standard zc admin page layout

---
+++ YOUR_ADMIN/includes/languages/english/extra_definitions/missing_images.php
@@ -0,0 +1,6 @@
+<?php
+
+// Define language definitions to avoid copy/pasting almost the same exact sentence.
+define('ERROR_IMAGE_FORMAT', 'The image is named as a %s file but instead registers as a %s! Try to resave and reupload your file in the proper format.');
+define('ERROR_FILE_NOT_FOUND', 'Image file does not exist. Edit the product\'s details and click "delete image" to remove from database.');
+define('ERROR_NOT_WEB_FORMAT', 'The file found is either not an image or not an image that can be inherently loaded in a web browser. Try reuploading the file in PNG, GIF, JPG, or BMP format.');
